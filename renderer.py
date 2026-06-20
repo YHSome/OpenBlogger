a2b3e9b8be811ffc47c88d92d352143f34f25dda
YHSome <949727232@qq.com>
2026-06-21 01:30:45 +0800

    友链头像 24→36px

--- renderer.py
+++ renderer.py
@@ -595,7 +595,7 @@ class BlogRenderer:
                 {
                     "title": "👥 好友链接",
                     "links": [
-                        {"name": "MKrari", "desc": "个人博客", "icon": "<img src=\"images/ksm1.webp\" style=\"width:24px;height:24px;border-radius:50%\">", "url": "https://mkrari.cn/"},
+                        {"name": "MKrari", "desc": "个人博客", "icon": "<img src=\"images/ksm1.webp\" style=\"width:36px;height:36px;border-radius:50%\">", "url": "https://mkrari.cn/"},
                     ],
                 },
                 {
